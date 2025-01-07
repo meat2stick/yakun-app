9d36593db4311fb397dba5ec6da8f93a63d22ede
Jordan Cheong <jordancheong520@gmail.com>
2025-01-07 21:19:55 +0800

    add context providers in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css'
+import React from "react";
+import {MenuContextProvider} from "./context/MenuContext.tsx";
+import {ItemModalContextProvider} from "./context/ItemModalContext.tsx";
+import Menu from "./component/Menu.tsx";
 
-function App() {
+
+const App: React.FC = () => {
 
     return (
-        <>
-        </>
+        <MenuContextProvider>
+            <ItemModalContextProvider>
+                <Menu/>
+            </ItemModalContextProvider>
+        </MenuContextProvider>
     )
 }
 
