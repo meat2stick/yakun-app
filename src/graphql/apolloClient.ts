5efccf85f1c277538604ddbdcaa4ef1fafd7bdef
Jordan Cheong <jordancheong520@gmail.com>
2025-01-08 22:25:37 +0800

    update server url

--- src/graphql/apolloClient.ts
+++ src/graphql/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-    uri: 'http://0.0.0.0:3000/graphql', // TODO: Make this into an env variable.
+    uri: 'http://13.214.137.214/graphql', // TODO: Make this into an env variable.
     cache: new InMemoryCache(),
 });
 
