75d79692f91b4d561dfc259e5ea5bc94ca61c8b5
Jordan Cheong <jordancheong520@gmail.com>
2025-01-08 20:22:31 +0800

    move context providers to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,17 @@ import './index.css'
 import App from './App.tsx'
 import client from "./graphql/apolloClient.ts";
 import {ApolloProvider} from "@apollo/client";
+import {MenuContextProvider} from "./context/MenuContext.tsx";
+import {ItemModalContextProvider} from "./context/ItemModalContext.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <ApolloProvider client={client}>
-            <App/>
-        </ApolloProvider>
+        <MenuContextProvider>
+            <ItemModalContextProvider>
+                <ApolloProvider client={client}>
+                    <App/>
+                </ApolloProvider>
+            </ItemModalContextProvider>
+        </MenuContextProvider>
     </StrictMode>,
 )
